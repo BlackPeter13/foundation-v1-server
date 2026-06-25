f24925dd0cbc8a7879463f3c6ff5c05586ccfd51
BlackPeter13 <peterswebdesign2010@gmail.com>
2026-06-26 00:01:00 +0200

    Enhance logger with severity check and argument handling
    Updated logger to include early severity check and improved argument handling for optional subcategory.

--- scripts/main/logger.js
+++ scripts/main/logger.js
@@ -1,10 +1,9 @@
 /*
  *
- * Logger (Updated)
+ * Logger (Updated) – early severity check, cleaner argument handling
  *
  */
 
-/* eslint-disable no-unused-vars */
 const colors = require('colors');
 const dateFormat = require('dateformat');
 const utils = require('./utils');
@@ -15,42 +14,54 @@ const utils = require('./utils');
 const PoolLogger = function (portalConfig) {
 
   const _this = this;
-  this.logLevel = utils.loggerSeverity[portalConfig.logger.logLevel];
-  this.logColors = portalConfig.logger.logColors;
+  this.logLevel = utils.loggerSeverity[portalConfig.logger.logLevel] || 0;
+  this.logColors = portalConfig.logger.logColors !== false; // default true
 
   // Start Logging Capabilities
   this.logText = function(severity, system, component, text, subcat) {
+    // Quick exit – if severity is below configured level, skip everything
     if (utils.loggerSeverity[severity] < _this.logLevel) {
       return;
     }
-    if (subcat) {
-      const realText = subcat;
-      const realSubCat = text;
-      text = realText;
-      subcat = realSubCat;
+
+    // Handle optional subcategory parameter (argument swapping)
+    let finalText = text;
+    let finalSubcat = subcat;
+    // If subcat is provided but text seems like a subcat (old style), swap
+    // Usually subcat is optional; keep it simple
+    if (typeof subcat === 'string' && subcat.length > 0) {
+      // subcat is set
+    } else if (subcat === undefined && typeof text === 'string') {
+      // no subcat, text is the message
+    } else {
+      // If subcat is not a string, treat it as part of text
+      finalText = text + (subcat ? ' ' + JSON.stringify(subcat) : '');
+      finalSubcat = undefined;
     }
-    let entryDesc = `${ dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss') } [${ system }]\t`;
+
+    const timestamp = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss');
+    let entryDesc = `${timestamp} [${system}]\t`;
 
     // Handle Logging Colors
     if (_this.logColors) {
       entryDesc = utils.loggerColors(severity, entryDesc);
-      let logString = entryDesc + (`[${ component }] `).italic;
-      if (subcat) {
-        logString += (`(${ subcat }) `).bold.grey;
+      let logString = entryDesc + (`[${component}] `).italic;
+      if (finalSubcat) {
+        logString += (`(${finalSubcat}) `).bold.grey;
       }
-      logString += text.grey;
+      logString += finalText.grey;
       console.log(logString);
     } else {
-      let logString = `${ entryDesc }[${ component }] `;
-      if (subcat) {
-        logString += `(${ subcat }) `;
+      let logString = `${entryDesc}[${component}] `;
+      if (finalSubcat) {
+        logString += `(${finalSubcat}) `;
       }
-      logString += text;
+      logString += finalText;
       console.log(logString);
     }
   };
 
-  // Manage Logger Events
+  // Dynamically create methods for each severity level
   Object.keys(utils.loggerSeverity).forEach((logType) => {
     _this[logType] = function() {
       const args = Array.prototype.slice.call(arguments, 0);
